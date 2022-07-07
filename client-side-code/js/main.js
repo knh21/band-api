f60c4595f6a33e979f99fd611e50f045ffa8ad89
Kyle Hill <kyle.hill56@yahoo.com>
2022-07-07 19:10:09 -0400

    fixed cors

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -1,1 +1,14 @@
-alert("working")
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://poppunk-bands-api.herokuapp.com/api/${name}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.origin
+    }catch(error){
+        console.log(error)
+    }
+}
